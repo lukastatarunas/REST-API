9db054d5983a744a4ebcbf43173899f40321c512
lukastatarunas <lukasttatarunas@gmail.com>
2019-08-23 11:20:34 +0300

    Refactored the whole code

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const request = require('request')
 const app = express()
 
 app.get('/', (req, res) => { res.send('It is working!') } )
-app.get('/api/v1/lrtlive',  (req, res) => {
+app.get('/api/v1/lrtlive', (req, res) => {
 res.header('Access-Control-Allow-Origin', '*')
 res.header('Access-Control-Allow-Headers', 'x-requested-with, x-requested-by')
-  request(`https://www.lrt.lt/static/tvprog/tvprog.json`,  (error, response, body) => {
-      if(error) res.send('Error occured while fetching LRT data!')
+  request(`https://www.lrt.lt/static/tvprog/tvprog.json`, (error, response, body) => {
+      if (error) res.send('Error occured while fetching LRT data!')
       else res.send(body)
   })
 })
 
 app.listen(process.env.PORT || 5500, () => {
-	console.log(`app is running on port ${process.env.PORT}`)
+	console.log(`app is running on port ${ process.env.PORT }`)
 })
